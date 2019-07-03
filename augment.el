bbbb9f0fc89349dd02af4de539e340b3bfecc38a
Jens Christian Jensen <jensecj@gmail.com>
2019-07-03 13:45:12 +0200

    initial commit

---
+++ augment.el
@@ -0,0 +1,18 @@
+;;; augment.el. --- -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019 Jens Christian Jensen
+
+;; Author: Jens Christian Jensen <jensecj@gmail.com>
+;; URL: https://github.com/jensecj/augment.el/
+;; Keywords: regexp, bug-reference
+;; Package-Requires: ((emacs "27.0.50"))
+;; Package-Version: 20190703
+;; Version: 0.1
+
+;;; Commentary:
+
+;;; Code:
+
+
+
+(provide 'augment)
