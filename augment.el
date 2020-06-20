c76c485f312665fcddc0bf6c1579f739119b4880
Jens C. Jensen <jensecj@posteo.net>
2020-06-20 14:40:06 +0200

    added sections and update package metadata

--- augment.el
+++ augment.el
@@ -1,17 +1,27 @@
 ;;; augment.el. --- -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019 Jens Christian Jensen
+;; Copyright (C) 2020 Jens Christian Jensen
 
 ;; Author: Jens Christian Jensen <jensecj@gmail.com>
 ;; URL: https://github.com/jensecj/augment.el/
-;; Keywords: regexp, bug-reference
-;; Package-Requires: ((emacs "27.0.50"))
-;; Package-Version: 20190703
+;; Keywords: reference, overlay, augmentation
+;; Package-Requires: ((emacs "28.0.50"))
+;; Package-Version: 20200620
 ;; Version: 0.1
 
 ;;; Commentary:
 
 ;;; Code:
+
+;;;; settings
+
+;;;; predicates
+
+;;;; core
+
+;;;; public
+
+;;;; mode
 
 
 
