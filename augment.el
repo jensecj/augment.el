c64cfdc07ecfb8343d146735576e5a9963d91836
Jens C. Jensen <jensecj@posteo.net>
2020-06-20 14:52:17 +0200

    add face for augmented sections; skeleton for augment(-prog)-mode, and some todos

--- augment.el
+++ augment.el
@@ -15,6 +15,9 @@
 
 ;;;; settings
 
+;; TODO: customize face per entry?
+(defface augment-face '((t (:inherit link)))
+  "Face used for augmented sections.")
 ;;;; predicates
 
 ;;;; core
@@ -23,6 +26,33 @@
 
 ;;;; mode
 
+(defvar augment-map
+  (let ((map (make-sparse-keymap)))
+    map)
+  "Keymap used by augment buttons.")
+
+;;;###autoload
+(define-minor-mode augment-mode
+  "Toggle augmenting the buffer."
+  nil " augment" augment-map
+  )
+
+(define-globalized-minor-mode global-augment-mode
+  augment-mode
+  (lambda () (augment-mode +1)))
+
+;;;###autoload
+(define-minor-mode augment-prog-mode
+  "Like `augment-mode', but only augment in comments and strings."
+  nil " augment-prog" augment-map
+  )
+
+(define-globalized-minor-mode global-augment-prog-mode
+  augment-prog-mode
+  (lambda () (augment-prog-mode +1)))
+
+;; TODO: benchmark -- what is a reasonable number of augmentation for a buffer?
+;; TODO: show augment in eldoc?
 
 
 (provide 'augment)
